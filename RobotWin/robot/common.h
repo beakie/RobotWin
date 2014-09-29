a7762d41cb2b90a81cab243d8d9b4bd6607af285
... <...>
2014-09-29 11:18:25 +0100

    ...

--- RobotWin/robot/common.h
+++ RobotWin/robot/common.h
@@ -13,10 +13,34 @@
 #include "common-vector2.h"
 #include "common-vector3.h"
 #include "common-vector4.h"
-#include "common_types.h"
 
 #include "common-projection3d.h"
 #include "common-space2d.h"
 #include "common-space3d.h"
+
+namespace Common
+{
+
+	typedef Matrix2<char> Matrix2c;
+	typedef Matrix2<int> Matrix2i;
+	typedef Matrix2<float> Matrix2f;
+
+	typedef Matrix3<char> Matrix3c;
+	typedef Matrix3<int> Matrix3i;
+	typedef Matrix3<float> Matrix3f;
+
+	typedef Matrix4<char> Matrix4c;
+	typedef Matrix4<int> Matrix4i;
+	typedef Matrix4<float> Matrix4f;
+
+	typedef Vector2<char> Vector2c;
+	typedef Vector2<int> Vector2i;
+	typedef Vector2<float> Vector2f;
+
+	typedef Vector3<char> Vector3c;
+	typedef Vector3<int> Vector3i;
+	typedef Vector3<float> Vector3f;
+
+}
 
 #endif // COMMON_H
